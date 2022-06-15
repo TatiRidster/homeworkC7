1b054c7329155628e6109e58ed340bc9a2244619
Tatiana <tatnovid@gmail.com>
2022-06-15 12:50:16 +0300

    двумерный массив вещественных чисел размером M*N

---
+++ ex47/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+void CreateArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().NextDouble() * 20 - 10;
+    }
+  }
+}
+
+void print2DArray(double[,] arrayToPrint)
+{
+    Console.Write($"[ ]\t");
+    const int startIndex = 65;
+    for (var i = startIndex + 0; i < startIndex + arrayToPrint.GetLength(1); i++)
+    {
+        Console.Write($"[{((char)i)}]\t");
+    }
+    Console.WriteLine();
+    for (var i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        Console.Write($"[" + i + "]\t");
+
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(arrayToPrint[i, j], 1) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вы задали двумерный массив размером {m}*{n}.");
+double[,] array = new double[m, n];
+CreateArray(array);
+print2DArray(array);
+Console.WriteLine();
